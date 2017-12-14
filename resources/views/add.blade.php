a08ab70e41f7ea45c8d592cc663df1b444fbd1f7
ValentinArnould <valentin.arnould@outlook.com>
2017-12-14 13:13:50 +0000

    ajout d'une page 404

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -3,8 +3,31 @@
 @section('compléments')
 @endsection
 
-@section('titre', 'JCRU2DR - Accueil')
+@section('titre', 'JCRU2DR - Ajouter')
 
 @section('main')
+
+  <h1> Ajout de contenu</h1>
+  <div class="container">
+    {{ Form::open(array('url' => '/', 'method' => 'get')) }}
+    <div class="form-group formul">
+      <a href="/add/race">
+        {{ Form::button("Race",array('class' => 'form-control')) }}
+      </a>
+      <a href="/add/classe">
+        {{ Form::button("Classe",array('class' => 'form-control')) }}
+      </a>
+      <a href="/add/compétence">
+        {{ Form::button("Compétence",array('class' => 'form-control')) }}
+      </a>
+      <a href="/add/don">
+        {{ Form::button("Don",array('class' => 'form-control')) }}
+      </a>
+      <a href="/add/custom">
+        {{ Form::button("Contenu personnalisé",array('class' => 'form-control')) }}
+      </a>
+    </div>
+    {{ Form::close() }}
+  </div>
 
 @endsection
