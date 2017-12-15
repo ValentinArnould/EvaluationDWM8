732cefb988a51cc6fc4aa77a19a1186ac3c3747f
ValentinArnould <valentin.arnould@outlook.com>
2017-12-15 13:56:51 +0000

    ouf, j'ai rien perdu

---
+++ app/Http/Controllers/Competence.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class Competence extends Controller
+{
+  public function ajouter()
+  {
+
+  }
+
+  public function supprimer()
+  {
+
+  }
+
+  public function modifier()
+  {
+
+  }
+}
